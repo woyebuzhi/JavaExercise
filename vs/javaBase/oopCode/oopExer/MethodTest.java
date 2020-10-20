e5d9cdea9b052490b872d8bce75d578dce02f1d6
woyebuzhi <492530524@qq.com>
2020-10-20 23:28:46 +0800

    方法的重载，递归函数，方法的值传递，没有做练习

---
+++ vs/javaBase/oopCode/oopExer/MethodTest.java
@@ -0,0 +1,53 @@
+package javaBase.oopCode.oopExer;
+
+
+/**
+ * MethodTest
+ */
+/* public class MethodTest {
+
+    public static void main(String[] args) {
+
+        Person obj = new Person();
+        obj.age = 5;
+        System.out.println("初始化" + obj.age);
+
+        change(obj);
+        System.out.println("修改" + obj.age);
+
+        
+        
+    }
+
+
+public static void change(Person obj) {
+            System.out.println("之前" + obj.age);
+            obj.age = 3;
+            System.out.println("之后" + obj.age);
+        }
+
+}
+class Person{
+
+    int age;
+} */
+
+/**
+ * MethodTest
+ */
+public class MethodTest {
+
+    public static void main(String[] args) {
+        
+        int x = 5;
+        System.out.println("初始" + x);
+
+        change(x);
+        System.out.println("之后" + x);
+    }
+
+    public static void change(int x){
+        x = 3;
+        System.out.println("change之后" + x);
+    }
+}
---
+++ vs/javaBase/oopCode/oopExer/RecursionTest.java
@@ -0,0 +1,37 @@
+//递归函数
+
+package javaBase.oopCode.oopExer;
+
+
+
+/**
+ * RecursionTest
+ */
+public class RecursionTest {
+
+    public static void main(String[] args) {
+
+        RecursionTest read = new RecursionTest();
+
+        int x = 3;
+        int sumNum = read.getSum(x);
+        System.out.println(sumNum);
+
+        
+            
+            
+        
+        
+    }
+
+    //递归方法
+    public int getSum(int n){
+
+        if(n == 1){
+            return 1;
+
+        }else {
+            return n + getSum(n - 1);
+        }
+    }
+}
